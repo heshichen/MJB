f042898c1b4461b4bbcd1be0c85fee737c6224b3
tianlongrenlufei <793308186@qq.com>
2017-11-01 10:47:03 +0800

    Initial Commit

---
+++ MJB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MJB
+//
+//  Created by admin on 2017/11/1.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
